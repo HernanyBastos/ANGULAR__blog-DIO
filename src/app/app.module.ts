0ae9316d0749641f3303cf5a044d98e2b50ca4e9
Hernany Bastos <hernanybastosfig@gmail.com>
2025-02-28 16:19:47 -0300

    Criação do Big-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentModule } from './components/component-module.module';
+import { HomeComponent } from './pages/home/home.component';
 
 
 //Componentes
@@ -12,6 +13,7 @@ import { ComponentModule } from './components/component-module.module';
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
 
   ],
   imports: [
